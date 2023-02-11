9550f3c5bff47172f720c0dc64e35e5c85563527
Dan <875126243@qq.com>
2023-02-11 20:06:49 +0800

    fix update send status

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,38 +1,13 @@
-
-<template>
-  <main>
-    <div id="chat_container">
-      <div
-        v-for="(chat, i) in wrapper"
-        :key="i"
-        class="wrapper"
-        :class="{ ai: chat.isAi }"
-      >
-        <Chat :chat="chat" :key="i" />
-      </div>
-    </div>
-    <form @submit.prevent="fetchAnswer">
-      <textarea
-        rows="1"
-        cols="1"
-        placeholder="Please enter your question..."
-        v-model="question"
-      ></textarea>
-      <button type="submit"><img src="@/assets/send.svg" alt="send" /></button>
-    </form>
-  </main>
-</template>
 <script setup>
 import { ref } from "vue";
 import Chat from "@/components/Chat.vue";
 const question = ref("");
 const wrapper = ref([]);
 const loading = ref(false);
-
+const baseUrl = "http://localhost:1573"
 document.onkeydown = function (e) {
    let key = (window).event.keyCode;
   if (key === 13) {
-     console.log('回车')
      if(question.value && question.value !== ""){
       fetchAnswer()
      }
@@ -51,7 +26,7 @@ const fetchAnswer = async () => {
       isAi: true,
       value: "Loading....",
     });
-    const res = await fetch("http://localhost:1573", {
+    const res = await fetch(baseUrl, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
@@ -79,3 +54,27 @@ const fetchAnswer = async () => {
  
 };
 </script>
+
+<template>
+  <main>
+    <div id="chat_container">
+      <div
+        v-for="(chat, i) in wrapper"
+        :key="i"
+        class="wrapper"
+        :class="{ ai: chat.isAi }"
+      >
+        <Chat :chat="chat" :key="i" />
+      </div>
+    </div>
+    <form @submit.prevent="fetchAnswer">
+      <textarea :disabled="loading"
+        rows="1"
+        cols="1"
+        placeholder="Please enter your question..."
+        v-model="question"
+      ></textarea>
+      <button type="submit"><img src="@/assets/send.svg" alt="send" /></button>
+    </form>
+  </main>
+</template>
